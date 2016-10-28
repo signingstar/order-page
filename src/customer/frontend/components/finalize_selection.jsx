38b9725546635753987a0bd27b5e07ad72e500fe
signingstar <anilkeshari@gmail.com>
2016-10-28 12:11:21 +0530

    Fix css and image count

--- src/customer/frontend/components/finalize_selection.jsx
+++ src/customer/frontend/components/finalize_selection.jsx
@@ -1,32 +1,12 @@
 import React from "react"
 import { TransitionMotion } from "react-motion"
 
-// const FinalizeSelection = ({ images }) => {
-//   const imageList = images.map(image => (
-//     <li key={image.id} className='nav-left'>
-//       {image.filename}
-//     </li>
-//   ))
-//
-//   return (
-//     <div className='finalize-page'>
-//       <div className='filters'>
-//         filter 1
-//       </div>
-//       <div className='images'>
-//         <ul>
-//           <li><div>Image Name</div><div>Qualified</div></li>
-//           {imageList}
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
 const FinalizeSelection = ({images, handleChange, willEnter, willLeave, getStyles, getDefaultStyles, value, handleSelect, selected}) => {
+  const styles = getStyles()
+
   return (
     <div className='finalize-page'>
-      <h3>Total Images: {images.length}</h3>
+      <h3>Total Images: {styles.length}</h3>
       <div className='filters fields'>
         <div className='field'>
           <input
@@ -49,7 +29,7 @@ const FinalizeSelection = ({images, handleChange, willEnter, willLeave, getStyle
       </div>
       <TransitionMotion
         defaultStyles={getDefaultStyles()}
-        styles={getStyles()}
+        styles={styles}
         willLeave={willLeave}
         willEnter={willEnter}
       >
